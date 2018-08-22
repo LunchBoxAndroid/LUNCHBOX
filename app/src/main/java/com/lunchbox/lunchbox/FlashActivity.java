ab13199ff49b167668c62a8b082e162accb9de75
nSpider <dipayanray3@gmail.com>
2018-08-23 02:40:02 +0530

    Change layout of FlashActivity

--- app/src/main/java/com/lunchbox/lunchbox/FlashActivity.java
+++ app/src/main/java/com/lunchbox/lunchbox/FlashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 //Function of this activity : To check if a user is signed in or not
 
@@ -12,8 +13,11 @@ public class FlashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_flash);
+
+        // For transparent notification and navigation bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         if (userLoggedIn()){
             // TODO:Change activity to main page
         } else {
