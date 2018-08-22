f11392e10ce30c2cc25f44acc1ccd706cb03defe
nSpider <dipayanray3@gmail.com>
2018-08-22 20:45:29 +0530

    Initial commit

---
+++ app/src/main/java/com/lunchbox/lunchbox/FlashActivity.java
@@ -0,0 +1,15 @@
+package com.lunchbox.lunchbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//Function of this activity : To check if a user is signed in or not
+
+public class FlashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+    }
+}
