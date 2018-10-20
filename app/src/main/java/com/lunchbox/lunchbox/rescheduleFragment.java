85f735bf50fe660a003190a477b27b3b6c99568f
Dipika98 <choudharydipika04@gmail.com>
2018-10-21 04:25:14 +0530

    added all blank fragments

---
+++ app/src/main/java/com/lunchbox/lunchbox/rescheduleFragment.java
@@ -0,0 +1,22 @@
+package com.lunchbox.lunchbox;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class rescheduleFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reschedule, container, false);
+    }
+
+}
