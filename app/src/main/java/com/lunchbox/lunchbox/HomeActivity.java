e825cd083df0d69e5ae014d543a57167509933ef
Ishan R Tripathi <1605279@kiit.ac.in>
2018-11-17 15:43:57 +0530

    Minor bug fixes

--- app/src/main/java/com/lunchbox/lunchbox/HomeActivity.java
+++ app/src/main/java/com/lunchbox/lunchbox/HomeActivity.java
@@ -3,6 +3,7 @@ package com.lunchbox.lunchbox;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -56,7 +57,7 @@ public class HomeActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView =findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         navigationView.setCheckedItem(R.id.nav_home);
@@ -127,15 +128,11 @@ public class HomeActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         Fragment fragment=null;
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            isOnHomeFragment = true;
-        } else {
-            isOnHomeFragment = false;
-        }
+        isOnHomeFragment = id == R.id.nav_home;
 
         if (id == R.id.nav_home) {
             fragment=new homeFragment();
